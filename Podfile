c165242acc4e3ad84422fc5dada75cbbae73d119
Adrian Tofan <adrian@tofan.co>
2013-03-14 17:59:24 +0100

    added cocoapods and mtdatrs

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios
+pod 'MTDates' , '~> 0.8.0'
--- src/Models/ATRecurrence.h
+++ src/Models/ATRecurrence.h
@@ -1,5 +1,12 @@
 #import "_ATRecurrence.h"
 
+typedef enum {
+  ATRecurrenceTypeDay       = 0,
+  ATRecurrenceTypeWeek      = 1,
+  ATRecurrenceTypeMonth     = 2,
+  ATRecurrenceTypeYear      = 3,
+} ATRecurrenceType; 
+
 @interface ATRecurrence : _ATRecurrence {}
 // Custom logic goes here.
 @end
